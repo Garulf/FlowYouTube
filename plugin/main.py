75335c857936f125b0922c768694c6147aa723b4
Garulf <535299+Garulf@users.noreply.github.com>
2022-02-15 17:11:35 -0500

    Add caching

--- plugin/main.py
+++ plugin/main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from flox import Flox, utils
 
 from youtube_search import YoutubeSearch
@@ -8,8 +9,16 @@ HIGH_QUALITY_THUMB = 'hq720'
 THUMB_EXT = 'jpg'
 MAX_THREADS = 10
 DEFAULT_SEARCH_LIMIT = 10
+TEN_MINUTES = 600
+MAX_CACHE_AGE = TEN_MINUTES
 
 class Youflowtube(Flox):
+
+    def _query(self, query):
+        if query != '':
+            path = Path(utils.gettempdir(), self.name)
+            self._results = utils.cache(query, max_age=MAX_CACHE_AGE, dir=path)(self.query)(query)
+        return self._results
 
     def query(self, query):
         limit = self.settings.get('max_search_results', DEFAULT_SEARCH_LIMIT)
@@ -30,9 +39,10 @@ class Youflowtube(Flox):
                     method=self.browser_open,
                     parameters=[url]
                 )
+        return self._results
 
     def context_menu(self, data):
         pass
 
 if __name__ == "__main__":
-    Youflowtube(debug=True)
+    Youflowtube()
