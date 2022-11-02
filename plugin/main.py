d03233bc4b23e50c1e0a0fc8974cc49f8c90e460
Garulf <535299+Garulf@users.noreply.github.com>
2022-11-02 05:41:47 -0400

    Add custom context menu for custom program

--- plugin/main.py
+++ plugin/main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from subprocess import Popen
 import json
 from flox import Flox, utils, ICON_BROWSER
 
@@ -15,11 +16,13 @@ MAX_CACHE_AGE = TEN_MINUTES
 LANGUAGES_FILE = 'languages.json'
 REGIONS_FILE = 'regions.json'
 
-def get_thumbnail(id:str, thumb_type:str=DEFAULT_THUMB, ext:str=THUMB_EXT):
+
+def get_thumbnail(id: str, thumb_type: str = DEFAULT_THUMB, ext: str = THUMB_EXT):
     """
     Get thumbnail url
     """
     return f'{THUMBNAIL_URL}/{id}/{thumb_type}.{THUMB_EXT}'
+
 
 class FlowYouTube(Flox):
 
@@ -32,18 +35,20 @@ class FlowYouTube(Flox):
             languages = json.load(f)
             self.language = languages[self.settings.get('language', 'Default')]
 
-
-
     def query(self, query):
         if query != '':
             path = Path(utils.gettempdir(), self.name)
-            cache_file = "_".join(filter(None, [query, self.language, self.region]))
-            self._results = utils.cache(cache_file, max_age=MAX_CACHE_AGE, dir=path)(self.search)(query)
+            cache_file = "_".join(
+                filter(None, [query, self.language, self.region]))
+            self._results = utils.cache(
+                cache_file, max_age=MAX_CACHE_AGE, dir=path)(self.search)(query)
         return self._results
 
     def search(self, query):
-        limit = int(self.settings.get('max_search_results', DEFAULT_SEARCH_LIMIT))
-        results = YoutubeSearch(query, max_results=limit, language=self.language, region=self.region).to_dict()
+        limit = int(self.settings.get(
+            'max_search_results', DEFAULT_SEARCH_LIMIT))
+        results = YoutubeSearch(
+            query, max_results=limit, language=self.language, region=self.region).to_dict()
         for item in results:
             with utils.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
                 self.result(item, executor)
@@ -55,9 +60,9 @@ class FlowYouTube(Flox):
         download_thumbs = self.settings.get('download_thumbs', True)
         if download_thumbs:
             icon = utils.get_icon(
-                get_thumbnail(item['id']), 
-                Path(self.name, 'thumbnails'), 
-                file_name=f'{item["id"]}.{THUMB_EXT}', 
+                get_thumbnail(item['id']),
+                Path(self.name, 'thumbnails'),
+                file_name=f'{item["id"]}.{THUMB_EXT}',
                 executor=executor
             )
         self.add_item(
@@ -77,6 +82,22 @@ class FlowYouTube(Flox):
             method=self.browser_open,
             parameters=[url]
         )
+        program_path = self.settings.get('program_path')
+        if program_path:
+            args = self.settings.get('program_args', '{url}').format(url=url)
+            title = f"Open with: {Path(program_path).name}"
+            subtitle = args
+            self.add_item(
+                title=title,
+                subtitle=subtitle,
+                icon=program_path,
+                method=self.open_in_program,
+                parameters=[program_path, args]
+            )
+
+    def open_in_program(self, program_path, args):
+        proc = Popen([program_path, args])
+
 
 if __name__ == "__main__":
     FlowYouTube()
